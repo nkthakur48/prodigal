06cd72b7aa6e56adf05ad87d33c6e6fe31aea36f
Nishant <nishant.thakur.ece@gmail.com>
2019-05-13 02:44:10 +0530

    Updates

---
+++ precache-manifest.e55c7133945615b9995d8c6c95902d8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "837267254153382f5785f27566da417e",
+    "url": "/prodigal/index.html"
+  },
+  {
+    "revision": "1846d0385d519ad6e499",
+    "url": "/prodigal/static/css/main.1d97ad97.chunk.css"
+  },
+  {
+    "revision": "7bda2b52a65e1e842863",
+    "url": "/prodigal/static/js/2.4c315cbb.chunk.js"
+  },
+  {
+    "revision": "1846d0385d519ad6e499",
+    "url": "/prodigal/static/js/main.124cdae6.chunk.js"
+  },
+  {
+    "revision": "91a53e68400b9fbbf81b",
+    "url": "/prodigal/static/js/runtime~main.b1feeb5f.js"
+  }
+]);
